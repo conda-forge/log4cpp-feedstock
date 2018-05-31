79c3a3cd1b8d4c14d1b56b67b7207b2a9ef16d97
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-05-31 19:29:23 +0200

    Test using cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-./configure --prefix="${PREFIX}"
-
 # Removing these folders because the `make check` is failing, it seems that it tries to build bcb5 and msvc. (it fails just for osx, however as this is not not necessary for linux I'm removing for both osx and linux)
 rm -R msvc6/*
 rm -R msvc7/*
 rm -R msvc10/*
 rm -R bcb5/*
+
+cmake --prefix="${PREFIX}" .
 
 make -j${CPU_COUNT}
 make check
